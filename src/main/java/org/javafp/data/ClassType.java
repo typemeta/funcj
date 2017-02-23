ac17d8f86a77834d5eefe50057358162ddb222a6
jon-hanson <jon.hanson@gmail.com>
2017-02-23 22:55:30 +0000

    Various minor additions and re-orgs.

---
+++ src/main/java/org/javafp/data/ClassType.java
@@ -0,0 +1,34 @@
+package org.javafp.data;
+
+import org.javafp.util.Functions.F;
+
+import java.util.Optional;
+
+public class ClassType<T> {
+
+    public static <T> ClassType<T> of(Class<T> clazz) {
+        return new ClassType<T>(clazz);
+    }
+
+    private final Class<T> clazz;
+
+    private ClassType(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    public Optional<T> cast(Object value) {
+        if (clazz.isAssignableFrom(value.getClass())) {
+            return Optional.of((T)value);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public <S, R> R cast(S value, F<T, R> success, F<S, R> fail) {
+        if (clazz.isAssignableFrom(value.getClass())) {
+            return success.apply((T)value);
+        } else {
+            return fail.apply(value);
+        }
+    }
+}
