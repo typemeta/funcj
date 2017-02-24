6d55945d6305d8c66b30ebed56f1e9fcf4740d35
jon-hanson <jon.hanson@gmail.com>
2017-02-24 08:09:26 +0000

    Added Validation & Predicates

--- src/main/java/org/javafp/control/Validation.java
+++ src/main/java/org/javafp/control/Validation.java
@@ -1,8 +1,9 @@
 package org.javafp.control;
 
 import org.javafp.data.IList;
-import org.javafp.util.Functions;
 import org.javafp.util.Functions.F;
+
+import java.util.function.Consumer;
 
 public interface Validation<E, T> {
     static <E, T> Validation<E, T> success(T result) {
@@ -13,18 +14,50 @@ public interface Validation<E, T> {
         return new Failure<E, T>(errors);
     }
 
+    static <E, T> Validation<E, T> failure(E error) {
+        return new Failure<E, T>(IList.of(error));
+    }
+
+    void handle(Consumer<Success<E, T>> success, Consumer<Failure<E, T>> failure);
+
+    <U> U match(F<Success<E, T>, U> success, F<Failure<E, T>, U> failure);
+
     <U> Validation<E, U> map(F<T, U> f);
 
-    class Success<E, T> implements Validation<E, T> {
-        public final T result;
+    <U> Validation<E, U> apply(Validation<E, F<T, U>> vf);
 
-        public Success(T result) {
-            this.result = result;
+    <U> Validation<E, U> flatMap(F<T, Validation<E, U>> f);
+
+    class Success<E, T> implements Validation<E, T> {
+        public final T value;
+
+        public Success(T value) {
+            this.value = value;
+        }
+
+        @Override
+        public void handle(Consumer<Success<E, T>> success, Consumer<Failure<E, T>> failure) {
+            success.accept(this);
+        }
+
+        @Override
+        public <U> U match(F<Success<E, T>, U> success, F<Failure<E, T>, U> failure) {
+            return success.apply(this);
         }
 
         @Override
         public <U> Validation<E, U> map(F<T, U> f) {
-            return null;
+            return success(f.apply(value));
+        }
+
+        @Override
+        public <U> Validation<E, U> apply(Validation<E, F<T, U>> vf) {
+            return vf.map(f -> f.apply(value));
+        }
+
+        @Override
+        public <U> Validation<E, U> flatMap(F<T, Validation<E, U>> f) {
+            return f.apply(value);
         }
     }
 
@@ -36,8 +69,32 @@ public interface Validation<E, T> {
         }
 
         @Override
+        public void handle(Consumer<Success<E, T>> success, Consumer<Failure<E, T>> failure) {
+            failure.accept(this);
+        }
+
+        @Override
+        public <U> U match(F<Success<E, T>, U> success, F<Failure<E, T>, U> failure) {
+            return failure.apply(this);
+        }
+
+        @Override
         public <U> Validation<E, U> map(F<T, U> f) {
-            return null;
+            return cast();
+        }
+
+        @Override
+        public <U> Validation<E, U> apply(Validation<E, F<T, U>> vf) {
+            return cast();
+        }
+
+        @Override
+        public <U> Validation<E, U> flatMap(F<T, Validation<E, U>> f) {
+            return cast();
+        }
+
+        public <U> Failure<E, U> cast() {
+            return (Failure<E, U>) this;
         }
     }
 }
