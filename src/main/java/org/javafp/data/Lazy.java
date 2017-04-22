52108bd8c0902f07a8d835b0f7ce97e62627998a
jon-hanson <jon.hanson@gmail.com>
2017-04-22 16:38:51 +0100

    Added Lazy.lift functions.

--- src/main/java/org/javafp/data/Lazy.java
+++ src/main/java/org/javafp/data/Lazy.java
@@ -1,25 +1,37 @@
 package org.javafp.data;
 
-import org.javafp.util.Functions;
+import static org.javafp.util.Functions.*;
 
 import java.util.Objects;
 
-public interface Lazy<T> extends Functions.F0<T> {
-    static <T> Lazy<T> of(Functions.F0<T> get) {
+public interface Lazy<T> extends F0<T> {
+    static <T> Lazy<T> of(F0<T> get) {
         return new LazyImpl<T>(get);
     }
 
-    static <T> Lazy<T> ofTS(Functions.F0<T> get) {
+    static <T> Lazy<T> ofTS(F0<T> get) {
         return new LazyThreadSafeImpl<T>(get);
+    }
+
+    static <A, R> F<Lazy<A>, Lazy<R>> lift(F<A, R> f) {
+        return lt -> Lazy.of(() -> f.apply(lt.apply()));
+    }
+
+    static <A, B, R> F2<Lazy<A>, Lazy<B>, Lazy<R>> lift(F2<A, B, R> f) {
+        return (la, lb) -> Lazy.of(() -> f.apply(la.apply(), lb.apply()));
+    }
+
+    static <A, B, C, R> F3<Lazy<A>, Lazy<B>, Lazy<C>, Lazy<R>> lift(F3<A, B, C, R> f) {
+        return (la, lb, lc) -> Lazy.of(() -> f.apply(la.apply(), lb.apply(), lc.apply()));
     }
 }
 
 class LazyImpl<T> implements Lazy<T> {
 
-    private class UnEval implements Functions.F0<T> {
-        final Functions.F0<T> source;
+    private class UnEval implements F0<T> {
+        final F0<T> source;
 
-        private UnEval(Functions.F0<T> source) {
+        private UnEval(F0<T> source) {
             this.source = source;
         }
 
@@ -31,9 +43,9 @@ class LazyImpl<T> implements Lazy<T> {
         }
     }
 
-    private Functions.F0<T> get;
+    private F0<T> get;
 
-    LazyImpl(Functions.F0<T> get) {
+    LazyImpl(F0<T> get) {
         this.get = new UnEval(Objects.requireNonNull(get));
     }
 
@@ -50,10 +62,10 @@ class LazyImpl<T> implements Lazy<T> {
 
 class LazyThreadSafeImpl<T> implements Lazy<T> {
 
-    private class UnEval implements Functions.F0<T> {
-        Functions.F0<T> source;
+    private class UnEval implements F0<T> {
+        F0<T> source;
 
-        private UnEval(Functions.F0<T> source) {
+        private UnEval(F0<T> source) {
             this.source = Objects.requireNonNull(source);
         }
 
@@ -70,9 +82,9 @@ class LazyThreadSafeImpl<T> implements Lazy<T> {
         }
     }
 
-    private Functions.F0<T> get;
+    private F0<T> get;
 
-    LazyThreadSafeImpl(Functions.F0<T> get) {
+    LazyThreadSafeImpl(F0<T> get) {
         this.get = new UnEval(Objects.requireNonNull(get));
     }
 
