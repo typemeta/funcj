a0a5f869cef2a75d4cc478107a06e179892e8aed
jon-hanson <jon.hanson@gmail.com>
2017-07-24 10:17:17 +0100

    Javadocs.

--- src/main/java/org/funcj/util/Reader.java
+++ src/main/java/org/funcj/util/Reader.java
@@ -3,6 +3,10 @@ package org.funcj.util;
 import org.funcj.data.IList;
 import org.funcj.util.Functions.F;
 
+/**
+ * The {@code Reader} class adds utility methods for using the function monad {@link org.funcj.util.Functions.F}
+ * as a Reader monad.
+ */
 public abstract class Reader {
     static <S> F<S, S> ask() {
         return F.of(s -> s);
