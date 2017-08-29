9ea4cf2f076754f9a4f22f6868005841c52956c7
jon-hanson <jon.hanson @gmail.com>
2017-08-29 22:14:15 +0100

    Javadocs

--- core/src/main/java/org/funcj/util/ClassType.java
+++ core/src/main/java/org/funcj/util/ClassType.java
@@ -5,11 +5,17 @@ import org.funcj.util.Functions.F;
 import java.util.Optional;
 
 /**
- * Utilities for casting and type-matching.
- * @param <T>
+ * A wrapper for {@link Class} that adds utility methods.
+ * @param <T>       the class type
  */
 public class ClassType<T> {
 
+    /**
+     * Construct a {@code ClassType} instance from a {@link Class}.
+     * @param clazz     the class
+     * @param <T>       the class type
+     * @return          the new {@code ClassType} instance
+     */
     public static <T> ClassType<T> of(Class<T> clazz) {
         return new ClassType<T>(clazz);
     }
@@ -20,6 +26,12 @@ public class ClassType<T> {
         this.clazz = clazz;
     }
 
+    /**
+     * If it has the same type then cast an object to this type and wrap in {@link Optional},
+     * otherwise return an empty {@code Optional}.
+     * @param value     the object to cast
+     * @return          an {@code Optional} that wraps the cast value
+     */
     public Optional<T> cast(Object value) {
         if (clazz.isInstance(value)) {
             return Optional.of((T)value);
@@ -28,6 +40,17 @@ public class ClassType<T> {
         }
     }
 
+    /**
+     * If the supplied object value has the same type as {@link ClassType#clazz},
+     * then cast to {@code T} and apply function {@code success},
+     * otherwise apply function {@code fail}.
+     * @param value     the object value to test
+     * @param success   function to be applied if the object value matches
+     * @param fail      function to be applied if the object value does not match
+     * @param <S>       the object value type
+     * @param <R>       the return type of both functions
+     * @return          the result of appllying either function
+     */
     public <S, R> R match(S value, F<T, R> success, F<S, R> fail) {
         if (clazz.isInstance(value)) {
             return success.apply((T)value);
--- core/src/main/java/org/funcj/util/Functors.java
+++ core/src/main/java/org/funcj/util/Functors.java
@@ -13,14 +13,26 @@ import static java.util.stream.Collectors.toList;
  */
 public abstract class Functors {
     /**
-     * Map a function over a {@link java.util.Collection}.
+     * Map a function over a {@link Collection}.
+     * @param f         the function to apply to each element of the collection
+     * @param ts        the collection
+     * @param <T>       the element type of the collection
+     * @param <U>       the return type of the function
+     * @return          a collection consisting of the results of applying the function to
+     *                  each element in the input collection
      */
     public static <T, U> List<U> map(F<T, U> f, Collection<T> ts) {
         return ts.stream().map(f::apply).collect(toList());
     }
 
     /**
-     * Map a function over a {@link java.lang.Iterable}.
+     * Map a function over a {@link Iterable}.
+     * @param f         the function to apply to each element provided by the {@code Iterable}
+     * @param iter      the iterable
+     * @param <T>       the element type of the iterable
+     * @param <U>       the return type of the function
+     * @return          a collection consisting of the results of applying the function to
+     *                  each element in the input iterable
      */
     public static <T, U> List<U> map(F<T, U> f, Iterable<T> iter) {
         return StreamSupport.stream(
@@ -30,7 +42,12 @@ public abstract class Functors {
     }
 
     /**
-     * Map a function over an {@link java.util.Optional}.
+     * Map a function over an {@link Optional}.
+     * @param f         the function to apply to the value within the {@code Optional}
+     * @param ot        the optional value
+     * @param <T>       the value type within the {@code Optional}
+     * @param <U>       the return type of the function
+     * @return          an {@code Optional} that contains the result of applying the function
      */
     public static <T, U> Optional<U> map(F<T, U> f, Optional<T> ot) {
         return ot.map(f::apply);
@@ -38,8 +55,14 @@ public abstract class Functors {
 
     /**
      * Map a function over an array.
+     * @param f         the function to apply to each element of the array
+     * @param from      the input array
+     * @param to        the output array
+     * @param <A>       the element type of the input array
+     * @param <B>       the element type of the output array
+     * @return          the output array
      */
-    public static <A, B> B[] map(A[] from, B[] to, F<A, B> f) {
+    public static <A, B> B[] map(F<A, B> f, A[] from, B[] to) {
         final int l = from.length;
         if (to.length != l) {
             final Class<?> type = to.getClass();
