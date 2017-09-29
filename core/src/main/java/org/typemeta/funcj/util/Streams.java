82bb193b97b2302bed72be7541aded93a03d3714
jon-hanson <jon.hanson@gmail.com>
2017-09-29 14:29:18 +0100

    Javadocs.

--- core/src/main/java/org/typemeta/funcj/util/Streams.java
+++ core/src/main/java/org/typemeta/funcj/util/Streams.java
@@ -5,6 +5,9 @@ import org.typemeta.funcj.data.Tuple2;
 import java.util.Map;
 import java.util.stream.Stream;
 
+/**
+ * Utility methods relating to {@link Stream}s.
+ */
 public abstract class Streams {
     /**
      * Create a stream of {@link Tuple2}s from a {@link Map}.
