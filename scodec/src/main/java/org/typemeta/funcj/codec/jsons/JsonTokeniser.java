a161087695804ce7318654257ced7202808b2684
jon.hanson@gmail.com <jon.hanson@gmail.com>
2018-09-06 08:18:03 +0100

    Fix Json tokenising bug.

--- scodec/src/main/java/org/typemeta/funcj/codec/jsons/JsonTokeniser.java
+++ scodec/src/main/java/org/typemeta/funcj/codec/jsons/JsonTokeniser.java
@@ -365,9 +365,10 @@ public class JsonTokeniser {
                     }
                 case I:
                     switch (c) {
-                        case 'e':
-                        case 'E':
+                        case '0': case '1': case '2': case '3': case '4':
+                        case '5': case '6': case '7': case '8': case '9': {
                             break;
+                        }
                         default: {
                             state = NumState.Z;
                             break;
