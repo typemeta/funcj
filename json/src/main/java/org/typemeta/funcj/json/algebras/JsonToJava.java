47e3afb504c56a6cd777d7fdbfaafbdc9fa2c56d
jon-hanson <jon.hanson@gmail.com>
2019-05-31 14:17:41 +0100

    null-fix to JsonToJava.

--- json/src/main/java/org/typemeta/funcj/json/algebras/JsonToJava.java
+++ json/src/main/java/org/typemeta/funcj/json/algebras/JsonToJava.java
@@ -1,15 +1,49 @@
 package org.typemeta.funcj.json.algebras;
 
+import org.typemeta.funcj.json.comb.JsonCombParser;
 import org.typemeta.funcj.json.model.JsonAlg;
 
 import java.util.*;
 
-public class JsonToJava implements JsonAlg<Object> {
+/**
+ * An algebra for converting JsValue objects to raw java data.
+ */
+class JsonToJava implements JsonAlg<Object> {
+    public static final Object NULL = new Object() {
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj == null || obj.equals(this);
+        }
+
+        @Override
+        public String toString() {
+            return "null";
+        }
+    };
+
+    /**
+     * Transform a JSON string into a raw Java equivalent.
+     * @param json      the JSON string
+     * @return          the raw Java value
+     */
+    public static LinkedHashMap<String, Object> toJava(String json) {
+        return JsonCombParser.parse(json)
+                .map(jsv -> (LinkedHashMap<String, Object>)jsv.apply(JsonToJava.INSTANCE))
+                .getOrThrow();
+    }
+
     public static final JsonToJava INSTANCE = new JsonToJava();
 
     @Override
     public Object nul() {
-        return null;
+        // We need to map a JsNull value to a NULL object (and not to a null value),
+        // as the algebra engine won't allow nulls.
+        return NULL;
     }
 
     @Override
