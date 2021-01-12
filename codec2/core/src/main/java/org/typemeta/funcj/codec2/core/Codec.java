2b6950fe86df3d02c8ee192fcc3e4044d4166945
jon hanson <jon.hanson@gmail.com>
2021-01-12 18:44:18 +0000

    Codec2

--- codec2/core/src/main/java/org/typemeta/funcj/codec2/core/Codec.java
+++ codec2/core/src/main/java/org/typemeta/funcj/codec2/core/Codec.java
@@ -1,17 +1,42 @@
 package org.typemeta.funcj.codec2.core;
 
+import org.typemeta.funcj.codec2.core.CodecFormat.EncodeResult;
+
 public interface Codec<T, IN, OUT> extends Encoder<T, OUT>, Decoder<T, IN> {
     @Override
     Class<T> type();
 
     @Override
-    OUT encode(EncoderCore<OUT> core, Context ctx, T value, OUT out);
+    default OUT encode(EncoderCore<OUT> core, Context ctx, T value, OUT out) {
+        final EncodeResult<OUT> nullEncRes = core.format().nullCodec().encode(core, ctx, value, out);
+        if (nullEncRes.encoded()) {
+            return nullEncRes.out();
+        } else {
+            final EncodeResult<OUT> dynEncRes = core.format().dynamicCodec().encodeDynamic(core, ctx, value, out, this);
+            if (dynEncRes.encoded()) {
+                return dynEncRes.out();
+            } else {
+                return encodeImpl(core, ctx, value, out);
+            }
+        }
+    }
 
     @Override
     OUT encodeImpl(EncoderCore<OUT> core, Context ctx, T value, OUT out);
 
     @Override
-    T decode(DecoderCore<IN> core, Context ctx, IN in);
+    default T decode(DecoderCore<IN> core, Context ctx, IN in) {
+        if (core.format().nullCodec().decode(core, ctx, in)) {
+            return null;
+        } else {
+            final T dynValue = core.format().dynamicCodec().decodeDynamic(core, ctx, in);
+            if (dynValue != null) {
+                return dynValue ;
+            } else {
+                return decodeImpl(core, ctx, in);
+            }
+        }
+    }
 
     @Override
     T decodeImpl(DecoderCore<IN> core, Context ctx, IN in);
